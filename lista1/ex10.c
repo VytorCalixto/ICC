5b8d52af6006e78dd09dd73243ca96f8dde96a44
VytorCalixto <vytorcalixto@gmail.com>
2015-10-25 16:25:27 -0200

    Corrigido ex10

--- lista1/ex10.c
+++ lista1/ex10.c
@@ -2,7 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
-#include "float_t.h"
+
+typedef union {
+    int32_t i;
+    float f;
+    // Bitfields
+    struct {
+        uint32_t mantissa: 23;
+        uint32_t exponent: 8;
+        uint32_t sign: 1;
+    } parts;
+}Float_t;
+
+void exploreFloat(Float_t ft) {
+    printf("Float: %1.15e\t Int: %i\t Mantissa: %i\t Expoente: %i\t Sinal:%i\n",
+            ft.f, ft.i, ft.parts.mantissa, ft.parts.exponent, ft.parts.sign);
+}
 
 void nextFive(float f) {
     Float_t ft;
@@ -13,13 +28,13 @@ void nextFive(float f) {
         ft.parts.mantissa += 1;
         exploreFloat(ft);
         float difference = fabsf(ft.f - f);
-        printf("Diferença entre %1.15f e %1.15f é %1.15f", ft.f, f, difference);
+        printf("Diferença entre %1.15e e %1.15e é %1.15e\n", ft.f, f, difference);
     }
 }
 
 int main() {
     float f;
-    scanf("Digite um número: %f", &f);
+    scanf("%f", &f);
     nextFive(f);
     return 0;
 }
--- lista1/float_t.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <float.h>
-#include "float.h"
-
-void exploreFloat(Float_t ft) {
-    printf("Float: %f\t Int: %i\t Mantissa: %i\t Expoente: %i\t Sinal:%i\n",
-            ft.f, ft.i, ft.parts.mantissa, ft.parts.exponent, ft.parts.sign);
-}
